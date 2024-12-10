51fa0b19dbcff0961de52422363f763eff7271e7
Leandro Hosken <lshv04@gmail.com>
2024-12-10 15:03:18 -0300

    trabalhando responsividade da primeira seção

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Navbar from './Navbar';
 import styles from './Section1.module.css'; // Importação do CSS Module
+import scrollImage from '../assets/scroll.png'; // Importação da imagem
 
 const Section1 = () => {
-  return (
-    <div className={`container-fluid g-0 ${styles.sectionContainer} g-0`}>
-        <Navbar/>
-    </div>
-  );
+    return (
+        <div className={`container-fluid g-0 ${styles.sectionContainer}`}>
+            <Navbar />
+            <div className={`container bord1  g-0`}>
+                <div className={` bord1 ${styles.section1main}  `}>
+                    <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur</h1>
+                    <p className={styles.paragraph}>
+                        Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis
+                        commodo aenean viverra
+                    </p>
+                    <div className={styles.imageContainer}>
+                        <img src={scrollImage} alt="Scroll" className={styles.image} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Section1;
