586034af309eb46dbe891086e9c4f9025b4830e2
Leandro Hosken <lshv04@gmail.com>
2024-12-11 19:30:32 -0300

    finalizado section4

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -8,11 +8,11 @@ const Section4 = () => {
         <section className={` container-fluid ${styles.sectionContainer}`}>
             <div className={` container bord ${styles.innerContainer}`}>
                 <div className={`row bord mx-auto ${styles.gridRow}`}>
-                    <div className={`col-12 col-md-9 bord ${styles.leftColumn}`}>
+                    <div className={`col-12 col-md-8 bord ${styles.leftColumn}`}>
                         <h2>Lorem ipsum dolor sit amet consectetur</h2>
                         <p>Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra</p>
                     </div>
-                    <div className={`col-12 col-md-3  bord ${styles.rightColumn}`}>
+                    <div className={`col-12 col-md-4  bord ${styles.rightColumn}`}>
                         <img src={image8} alt="Imagem 8" className={styles.image} />
                      
                     </div>
