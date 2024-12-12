dbeb9062706bb52befe87f042f8c576ba1ff56fb
Leandro Hosken <lshv04@gmail.com>
2024-12-12 11:39:16 -0300

    trabalhando imagem section7

--- src/components/Section7.jsx
+++ src/components/Section7.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from './Section7.module.css'; // Importa o CSS Module
+import image9 from '../assets/Section7/image9.png'; // Importa a imagem
 
 const Section7 = () => {
     return (
         <footer className={`container-fluid ${styles.footerContainer}`}>
-            <div className={`container bord ${styles.innerContainer}`}>
+            <div className={`container bord1 ${styles.innerContainer}`}>
                 <div className={`row ${styles.gridRow}`}>
-                    <div className={`col-12 col-md-3 bord ${styles.gridColumn}`}>
-                        {/* Conteúdo da primeira coluna */}
+                    <div className={`col-12 col-md-4 bord1 ${styles.gridColumn}`}>
+                        <img
+                            src={image9}
+                            alt="Descrição da imagem"
+                            className={styles.image}
+                        />
                     </div>
-                    <div className={`col-12 col-md-9 bord ${styles.gridColumn}`}>
+                    <div className={`col-12 col-md-8 bord1 ${styles.gridColumn}`}>
                         {/* Conteúdo da segunda coluna */}
                     </div>
                 </div>
